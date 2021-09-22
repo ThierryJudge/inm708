6fabfa3e033ab8ec4f683e4b61b0ea7d38518cfa
Thierry Judge <tejudge@gmail.com>
2021-09-22 10:56:11 -0400

    add histograms

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ for name, file_config in config.items():
     file_data = nib.load(file_config['path'])
     img = file_data.get_fdata()
 
+    print(bcolors.WARNING + "Question 2.a" + bcolors.ENDC)
+    print(f"Image size: {img.shape}")
+    print(f"Voxel size: {file_data.header.get_zooms()}")
+
     print(bcolors.WARNING + "Question 2.b" + bcolors.ENDC)
     print(bcolors.OKBLUE + "Michelson contrast" + bcolors.ENDC + ": ")
     print(michelson_contrast(img))
@@ -34,8 +38,8 @@ for name, file_config in config.items():
             snr = SNR(img, snr_config['fg'], snr_config['bg'], snr_config['window_size'])
             print(f"{snr_region} SNR {snr}")
 
-    print(bcolors.WARNING + "Question 3" + bcolors.ENDC)
     if 'filter' in file_config.keys():
+        print(bcolors.WARNING + "Question 3" + bcolors.ENDC)
         nb_filters = len(file_config['filter'].keys())
         fig = plt.figure(figsize=(10, 10))
 
@@ -45,6 +49,17 @@ for name, file_config in config.items():
         plt.suptitle(name)
         plt.imshow(img[:, :, slice], cmap='gray')
         plt.title("Original")
+
+        fig.add_subplot(nb_filters, 3, 4)
+        plt.hist(img.flatten(), bins=50)
+        plt.title("Histogram")
+
+        if 'nlmeans' in file_config['filter'].keys():
+            mask_value = file_config['filter']['nlmeans']['mask_value']
+            fig.add_subplot(nb_filters, 3, 7)
+            plt.hist(img[img > mask_value].flatten(), bins=50)
+            plt.title(f"Masked Histogram with mask > {mask_value}")
+
         i = 2
         for filter_name, filter_config in file_config['filter'].items():
             filtered = filters[filter_name](img, **filter_config)
@@ -63,5 +78,7 @@ for name, file_config in config.items():
                 for snr_region, snr_config in file_config['snr'].items():
                     snr = SNR(filtered, snr_config['fg'], snr_config['bg'], snr_config['window_size'])
                     print(f"{filter_name} {snr_region} SNR {snr}")
+
+        plt.savefig(f"{name}_filters.png")
 
 plt.show()
