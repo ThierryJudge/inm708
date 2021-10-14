59219000cfd294c82c8ecb2f52fb0c090776a566
Andreas Duquenne <andreas_duquenne@hotmail.com>
2021-10-13 20:33:19 -0400

    nothing

--- TP2/histogram.py
+++ TP2/histogram.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import numpy as np
 import imageio
 from matplotlib import pyplot as plt
+import seaborn as sns
+
+from utils import *
 
 
 def histogram(i, j, bins=10):
@@ -18,7 +21,10 @@ if __name__ == '__main__':
     parser.add_argument("path", type=Path, help='Path to the data folder')
     parser.add_argument("--bins", type=int, help='Number of bins', default=100)
     parser.add_argument("--log_scale", action='store_true', help='Use log scale')
+    parser.add_argument("--remove_black", type=int, help='Use seaborn?')
     args = parser.parse_args()
+
+    print( "\n" + bcolors.OKBLUE + "Informations: " + bcolors.ENDC + "\n")
 
     for i in range(1, 7):
         try:
@@ -32,13 +38,19 @@ if __name__ == '__main__':
 
         hist = histogram(I, J, args.bins)
 
-        print(f"Histogram sum {np.sum(hist)}, Number of pixels: {np.prod(I.shape)}")
+        print(bcolors.UNDERLINE + f'\nPaire {i}:' + bcolors.ENDC)
+        pretty_print("Histogram sum:", np.sum(hist))
+        pretty_print("Number of pixels:", np.prod(I.shape))
+
         assert np.sum(hist) == np.prod(I.shape)
 
         if args.log_scale:
             hist = np.log(hist)
 
-        print(f'Paire {i}: SSD : {ssd(I, J)}, Corr.: {cr(I, J)}, MI: {mi(I, J, args.bins)}')
+        pretty_print("SSD:", ssd(I, J))
+        pretty_print("Correlation:", cr(I, J))
+        pretty_print("MI:", mi(I, J, args.bins))
+
 
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
         ax1.imshow(I)
@@ -47,4 +59,14 @@ if __name__ == '__main__':
         ax2.set_title(f"J{i}")
         ax3.imshow(hist, origin='lower')
         ax3.set_title("H")
+
+        if args.remove_black:
+            sns.set_theme(style="ticks")
+
+            im1, im2 = flatten_rem_black(I, J,
+                                         int(np.prod(I.shape)*args.remove_black/100))
+            sns.jointplot(x=im1, y=im2,
+                          kind="hex", color="#4CB391",
+                          marginal_kws=dict(bins=args.bins,
+                                            fill=True))
         plt.show()
---
+++ TP2/utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def flatten_rem_black(im1, im2, n):
+    assert im1.shape == im2.shape
+
+    return np.sort(im1.flatten())[n:np.prod(im1.shape)], np.sort(
+        im2.flatten())[n:np.prod(im2.shape)]
+
+
+def pretty_print(s, value):
+    print(s + (30-len(s))*" " + f"{value}")
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
