6ee8518d21ef547b970a0408f2b0ef081f21ca46
thierryjudge <thierry.judge@usherbrooke.ca>
2021-10-19 16:25:22 -0400

    Fix recalage.py

---
+++ TP2/optimizer.py
@@ -0,0 +1,70 @@
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Tuple, List
+
+import imageio
+from scipy.ndimage import affine_transform
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+
+class Optimizer:
+    def __init__(self, eps: float):
+        self.eps = eps
+        self.variable_history = None
+
+    def step(self, variables: np.ndarray, grads: np.ndarray) -> np.ndarray:
+        if self.variable_history is None:
+            self.variable_history = variables[None]
+        else:
+            self.variable_history = np.concatenate([self.variable_history, variables[None]], axis=0)
+
+    def plot_variables(self, show=False, names: List = None):
+        plt.figure()
+        plt.plot(self.variable_history)
+        if names:
+            plt.legend(names)
+        if show:
+            plt.show()
+
+
+class GradientDescent(Optimizer):
+    def __init__(self, eps: float, momentum: float = 0):
+        super().__init__(eps)
+        self.momentum = momentum
+        self.v = None
+
+    def step(self, variables: np.ndarray, grads: np.ndarray) -> np.ndarray:
+        super(GradientDescent, self).step(variables, grads)
+
+        if self.v is None:
+            self.v = np.zeros(len(variables))
+
+        self.v = self.momentum * self.v + self.eps * grads
+        variables = variables - self.v
+
+        return variables
+
+
+class Adam(Optimizer):
+    def __init__(self, eps, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        super().__init__(eps)
+        self.m, self.v = 0, 0
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.t = 1
+
+    def step(self, variables: np.ndarray, grads: np.ndarray) -> np.ndarray:
+        super(Adam, self).step(variables, grads)
+
+        self.m = self.beta1 * self.m + (1 - self.beta1) * grads
+        self.v = self.beta2 * self.v + (1 - self.beta2) * (grads ** 2)
+
+        m_corr = self.m / (1 - self.beta1 ** self.t)
+        v_corr = self.v / (1 - self.beta2 ** self.t)
+
+        variables = variables - self.eps * (m_corr / (np.sqrt(v_corr) + self.epsilon))
+
+        return variables
--- TP2/recalage.py
+++ TP2/recalage.py
@@ -1,110 +1,107 @@
-from collections.abc import Sequence
 from pathlib import Path
-from typing import Tuple
 
 import imageio
-from scipy.ndimage import affine_transform
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
+from optimizer import GradientDescent, Optimizer, Adam
+from scipy.ndimage import affine_transform
+from similarity import ssd
 from tqdm import tqdm
 
-from similarity import ssd
+
+def recalage_translation(i, j, optimizer: Optimizer, iterations=500):
+    errors = []
+    variables = np.ones(2)  # p, q
+    for _ in tqdm(range(iterations)):
+        i_trans = translate(i, -variables[0], -variables[1])
+        errors.append(ssd(i_trans, j))
+
+        dx = np.gradient(i_trans, axis=0)
+        dy = np.gradient(i_trans, axis=1)
+
+        dp = 2 * np.sum((i_trans - j) * dx)
+        dq = 2 * np.sum((i_trans - j) * dy)
+
+        gradients = np.array([dp, dq])
+
+        variables = optimizer.step(variables, gradients)
+
+    print(variables)
+
+    return variables, errors
+
+
+def recalage_rotation(i, j, optimizer: Optimizer, iterations=500):
+    errors = []
+    variables = np.zeros(1)  # p, q
+    for _ in tqdm(range(iterations)):
+        i_trans = rotate(i, -variables[0])
+        errors.append(ssd(i_trans, j))
+
+        dx = np.gradient(i_trans, axis=0)
+        dy = np.gradient(i_trans, axis=1)
+
+        x = np.arange(j.shape[0])
+        y = np.arange(j.shape[1])
+        sin = np.sin(variables[0])
+        cos = np.cos(variables[0])
+        dtheta = 2 * np.sum((i_trans - j) * (dx * (-x * sin - y * cos) + dy * (x * cos - y * sin)))
+
+        gradients = np.array([dtheta])
+
+        variables = optimizer.step(variables, gradients)
+
+    print(variables)
+
+    return variables, errors
+
+
+def recalage_rigide(i, j, optimizer: Optimizer, iterations=500):
+    errors = []
+    variables = np.zeros(3)  # p, q, theta
+    for _ in tqdm(range(iterations)):
+        i_trans = rigid(i, *tuple(-variables))
+        errors.append(ssd(i_trans, j))
+
+        dx = np.gradient(i_trans, axis=0)
+        dy = np.gradient(i_trans, axis=1)
+
+        x = np.arange(j.shape[0])
+        y = np.arange(j.shape[1])
+        sin = np.sin(variables[2])
+        cos = np.cos(variables[2])
+        dtheta = 2 * np.sum((i_trans - j) * (dx * (-x * sin - y * cos) + dy * (x * cos - y * sin)))
+
+        dp = 2 * np.sum((i_trans - j) * dx)
+        dq = 2 * np.sum((i_trans - j) * dy)
+
+        gradients = np.array([dp, dq, dtheta])
+
+        variables = optimizer.step(variables, gradients)
+
+    print(variables)
+
+    return variables, errors
 
 
 def translate(img, p, q):
     mat = np.eye(3, 3)
     mat[0, 2] = p
     mat[1, 2] = q
-
     return affine_transform(img, mat)
-
-
-# class Optimizer:
-#     def __init__(self, eps: float):
-#         self.eps = eps
-#
-#     def step(self, variables: Sequence, grads: Sequence) -> Tuple:
-#         raise NotImplementedError
-#
-#
-# class GD(Optimizer):
-#
-#     def step(self, variables: np.ndarray, grads: np.ndarray) -> np.ndarray:
-#         variables = variables + self.eps * grads
-#         return variables
-#
-#
-# class GD_Momentum(Optimizer):
-#     def __init__(self, eps: float, momentum: float):
-#         super().__init__(eps)
-#         self.momentum = momentum
-#         self.v = None
-#
-#     def step(self, variables: np.ndarray, grads: np.ndarray) -> np.ndarray:
-#         if self.v is None:
-#             self.v = np.zeros(len(variables))
-#
-#         self.v = self.momentum * self.v + self.eps * g
-#         variables = variables + self.v
-#
-#         return variables
-
-
-def recalage_translation(i, j, iterations=500, eps=1e-9):
-    errors = []
-    dps, dqs = [], []
-    ps, qs = [], []
-    p, q = 1, 1
-    for _ in tqdm(range(iterations)):
-        i_trans = translate(i, -p, -q)
-        errors.append(ssd(i_trans, j))
-
-        # dx = np.diff(i_trans, axis=0, prepend=0)
-        # dy = np.diff(i_trans, axis=1, prepend=0)
-
-        dx = np.gradient(i_trans, axis=0)
-        dy = np.gradient(i_trans, axis=1)
-
-        # dx = translate(dx, p, q)
-        # dy = translate(dy, p, q)
-
-        dp = 2 * np.sum((i_trans - j) * dx)
-        dq = 2 * np.sum((i_trans - j) * dy)
-
-        # print(p, q)
-        # print(dp, dq)
-
-        p -= eps * dp
-        q -= eps * dq
-
-        ps.append(p)
-        qs.append(q)
-        dps.append(dp)
-        dqs.append(dq)
-
-        # plt.figure()
-        # plt.imshow(i_trans - i)
-        # plt.show()
-
-    print(p, q)
-
-    plt.figure()
-    plt.plot(ps)
-    plt.plot(qs)
-
-    plt.figure()
-    plt.plot(dps)
-    plt.plot(dqs)
-
-    return p, q, errors
 
 
 def rotate(img, theta):
     mat = np.array([[np.cos(theta), -np.sin(theta), 0],
                     [np.sin(theta), np.cos(theta), 0],
                     [0, 0, 1]])
-    print(mat)
+    return affine_transform(img, mat)
 
+
+def rigid(img, p, q, theta):
+    mat = np.array([[np.cos(theta), -np.sin(theta), p],
+                    [np.sin(theta), np.cos(theta), q],
+                    [0, 0, 1]])
     return affine_transform(img, mat)
 
 
@@ -116,18 +113,33 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     I = imageio.imread(args.path)
-    I2 = translate(I, 20, 10)
+    # I2 = translate(I, 20, 10)
+    # I2 = rotate(I, np.deg2rad(10))
+    I2 = rigid(I, 20, 10, np.deg2rad(10))
 
-    p, q, errors = recalage_translation(I2, I, 1000, eps=1e-8)
+    optim = Adam(eps=1e-4)
 
-    I3 = translate(I2, -p, -q)
+    variables, errors = recalage_rigide(I2, I, optim, 10000)
+    optim.plot_variables(names=['p', 'q', 'theta'])
+    I3 = rigid(I2, *tuple(-variables))
+
+    # variables, errors = recalage_rotation(I2, I, optim, 1000)
+    # optim.plot_variables(names=['theta'])
+    # I3 = rotate(I2, -variables[0])
+
+    # variables, errors = recalage_translation(I2, I, optim, 1000)
+    # optim.plot_variables(names=['p', 'q'])
+    # I3 = translate(I2, -variables[0], -variables[1])
 
     plt.figure()
     plt.plot(errors)
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     ax1.imshow(I)
+    ax1.set_title("Initial")
     ax2.imshow(I2)
+    ax2.set_title("Transformed")
     ax3.imshow(I3)
+    ax3.set_title("Registered")
 
     plt.show()
