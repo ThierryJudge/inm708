ad3f20cf07cc64ae819f558ce26966b4b12dd209
Andreas Duquenne <andreas_duquenne@hotmail.com>
2021-12-01 21:05:41 -0500

    segment

--- TP3/functionnal.py
+++ TP3/functionnal.py
@@ -19,7 +19,7 @@ from utils import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--fmri_path", type=str)
-    parser.add_argument("--ss", type=str, default="kmeans")
+    parser.add_argument("--ss", type=str, default="histogram")
     parser.add_argument("--t1_path", type=str)
     parser.add_argument("--ideal_path", type=str)
     parser.add_argument('--plot', dest='plot', action='store_true')
@@ -42,6 +42,11 @@ if __name__ == "__main__":
         t1 = nib.load("Data/t1.nii")
 
     img_fmri = fmri.get_fdata().squeeze()
+    # img_fmri_t0 = img_fmri[..., 10]
+    # new_image = nib.Nifti1Image(img_fmri_t0, affine=fmri.affine, header=fmri.header)
+    # nib.save(new_image, os.path.join('produced_im', 'frmi_t10.nii.gz'))
+
+
     img_t1 = t1.get_fdata().squeeze()
 
     print(f"Image size: {img_fmri.shape}")
@@ -72,8 +77,8 @@ if __name__ == "__main__":
     ideal = ideal[3:]
 
     # ------------- 2.2 Skull stripping based on histogram of t1 ------------ #
-    if args.ss == "histogram":
-        img_fmri[img_fmri < 0.1] = 0
+    # if args.ss == "histogram":
+    #     img_fmri[img_fmri < 0.1] = 0
 
     # ----------------- 2.2.bis Skull stripping with Kmeans ----------------- #
     # if args.ss == "kmeans":
@@ -88,12 +93,14 @@ if __name__ == "__main__":
     #     BM = np.min([np.mean(c1), np.mean(c2), np.mean(c3)])
     #
     #     if np.mean(c1) == BM:
-    #         img_fmri[img_fmri < np.max(c1)] = 0
+    #         img_fmri[img_fmri < np.mean(c1)] = 0
     #     elif np.mean(c2) == BM:
-    #         img_fmri[img_fmri < np.max(c2)] = 0
+    #         img_fmri[img_fmri < np.mean(c2)] = 0
     #     else:
-    #         img_fmri[img_fmri < np.max(c3)] = 0
-
+    #         img_fmri[img_fmri < np.mean(c3)] = 0
+    #
+    # print(img_fmri.shape)
+    # print(np.sum(img_fmri))
 
     # --------------------- 2.3 Check signal for a voxel --------------------- #
     # for i in [10, 20, 30, 40]:
@@ -118,7 +125,7 @@ if __name__ == "__main__":
     #                                img_fmri.shape[2],
     #                                1))
 
-    corr_threshold = 0.1
+    corr_threshold = 0.5
 
     def corr(a):
         if np.sum(a) != 0:
@@ -129,9 +136,8 @@ if __name__ == "__main__":
     corr_values = np.apply_along_axis(corr, 3, img_fmri)
 
     # --------------------------- 3.2 Segmentation --------------------------- #
-
-    corr_values[corr_values < 0.5] = 0
-    # Viewer(corr_values)
+    print(corr_values)
+    corr_values[corr_values < corr_threshold] = 0
     print(np.sum(corr_values))
     print(np.mean(corr_values))
 
@@ -142,28 +148,75 @@ if __name__ == "__main__":
     plt.imsave('produced_im/proj_coronal.png', np.sum(corr_values, axis=1), cmap='magma')
     plt.imsave('produced_im/proj_axial.png', np.sum(corr_values, axis=2), cmap='magma')
 
-    a = np.where(corr_values > corr_threshold)
+    a = np.where(corr_values >= corr_threshold)
     indexes_ = np.zeros((len(a[0]), 4))
     for i in range(len(a)):
         indexes_[:, i] = np.array(a[i])
     indexes_ = indexes_[:, :3]
     print(indexes_)
 
+    # !!!!!!!!!!!!!!!!!!! Try increase zone
+    # for ind in indexes_:
+    #     val = corr_values[int(ind[0]), int(ind[1]), int(ind[2])]
+    #     if val != 0:
+    #         corr_values[int(ind[0]), int(ind[1]), int(ind[2])+1] = val
+    #         corr_values[int(ind[0]), int(ind[1])+1, int(ind[2])] = val
+    #         corr_values[int(ind[0]), int(ind[1])+1, int(ind[2])+1] = val
+    #         corr_values[int(ind[0])+1, int(ind[1]), int(ind[2])] = val
+    #         corr_values[int(ind[0])+1, int(ind[1]), int(ind[2])+1] = val
+    #         corr_values[int(ind[0])+1, int(ind[1])+1, int(ind[2])] = val
+    #         corr_values[int(ind[0])+1, int(ind[1])+1, int(ind[2])+1] = val
+    #
+    # a = np.where(corr_values > corr_threshold)
+    # indexes_ = np.zeros((len(a[0]), 4))
+    # for i in range(len(a)):
+    #     indexes_[:, i] = np.array(a[i])
+    # indexes_ = indexes_[:, :3]
+    # print(indexes_)
+
+    # !!!!!!!!!!!!!!!!!!!
+
     # -------------------------- 3.3 Post Processing ------------------------- #
 
     # Attributing each point to a cluster
-    clustering = DBSCAN(eps=2, min_samples=5).fit(indexes_)
+    clustering = DBSCAN(eps=6, min_samples=12).fit(indexes_)
     print(clustering.labels_)
+    clustering.labels_ += 1
 
-    clust_ = np.full(corr_values.shape, -1)
+    clust_ = np.full(corr_values.shape, 0, dtype=np.uint16)
     for i in range(indexes_.shape[0]):
         clust_[int(indexes_[i, 0]), int(indexes_[i, 1]), int(indexes_[i, 2])] =\
             int(clustering.labels_[i])
 
-    clust_ += 1
-    # Viewer(clust_)
-    new_image = nib.Nifti1Image(corr_values, affine=np.eye(4))
-    nib.save(new_image, os.path.join('produced_im', 'seg.nii.gz'))
+    new_image = nib.Nifti1Image(clust_, affine=fmri.affine, header=fmri.header)
+    new_image.set_data_dtype(np.uint16)
+    nib.save(new_image, os.path.join('produced_im', 'seg2.nii.gz'))
+
+    # !!!!!!!!!!!!!!!!! Try make cube from cluster
+    f_ = np.zeros_like(clust_)
+    print(np.unique(clustering.labels_))
+    for cluster_i in (np.unique(clustering.labels_)):
+        if cluster_i != 0:
+            cluster_i_indexes = indexes_[np.where(clustering.labels_ == cluster_i)]
+            max_x = int(max(cluster_i_indexes[:, 0]))
+            min_x = int(min(cluster_i_indexes[:, 0]))
+            max_y = int(max(cluster_i_indexes[:, 1]))
+            min_y = int(min(cluster_i_indexes[:, 1]))
+            max_z = int(max(cluster_i_indexes[:, 2]))
+            min_z = int(min(cluster_i_indexes[:, 2]))
+
+            print('\n\n')
+            print(cluster_i)
+            print(cluster_i_indexes)
+            print(f"{min_x}, {max_x}")
+            print(f"{min_y}, {max_y}")
+            print(f"{min_z}, {max_z}")
+
+            f_[min_x:max_x, min_y:max_y, min_z:max_z] = cluster_i
+
+    new_image = nib.Nifti1Image(f_, affine=fmri.affine, header=fmri.header)
+    new_image.set_data_dtype(np.uint16)
+    nib.save(new_image, os.path.join('produced_im', 'seg_cube_like2.nii.gz'))
 
     plt.imsave('produced_im/seg_proj_sagittal.png', np.sum(clust_, axis=0), cmap='gray')
     plt.imsave('produced_im/seg_proj_coronal.png', np.sum(clust_, axis=1), cmap='gray')
